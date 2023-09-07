3454fea13ee360daeca53439a24ce738cf80107e
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2023-09-07 17:29:20 +0600

    switcher design updated

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,7 +3,7 @@ import ThemeSwitcher from './ThemeSwitcher.vue';
 </script>
 
 <template>
-    <header class="bg-gray-300 dark:bg-white px-10 py-3 flex items-center justify-between">
+    <header class="bg-gray-300 border-b border-gray-300 dark:bg-black px-10 py-3 flex items-center justify-between">
         <div>
             <router-link to="/" class="text-blue-600 font-semibold text-2xl">LOGO</router-link>
         </div>
@@ -12,13 +12,13 @@ import ThemeSwitcher from './ThemeSwitcher.vue';
 
         <ul class="flex justify-end items-center gap-4">
             <li>
-                <router-link to="/" class="text-base font-medium transition-all" active-class="text-blue-600">Home</router-link>
+                <router-link to="/" class="text-base font-medium transition-all dark:text-white" active-class="!text-blue-600">Home</router-link>
             </li>
             <li>
-                <router-link to="/about" class="text-base font-medium transition-all" active-class="text-blue-600">About</router-link>
+                <router-link to="/about" class="text-base font-medium transition-all dark:text-white" active-class="!text-blue-600">About</router-link>
             </li>
             <li>
-                <router-link to="/contact" class="text-base font-medium transition-all" active-class="text-blue-600">Contact</router-link>
+                <router-link to="/contact" class="text-base font-medium transition-all dark:text-white" active-class="!text-blue-600">Contact</router-link>
             </li>
         </ul>
     </header>
